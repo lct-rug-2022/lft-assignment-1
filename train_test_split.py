15333df4aea5eb576974596052e1ca5df33197d5
Konstantin Chernyshev <k4black@ya.ru>
2022-09-18 17:28:15 +0200

    refactor: split file

--- train_test_split.py
+++ train_test_split.py
@@ -1,45 +1,32 @@
 #!/usr/bin/env python
+"""Dataset train/test split script"""
+from pathlib import Path
 
-
-import argparse
-
+import click
 from sklearn.model_selection import train_test_split
 
-from utils import read_corpus
 
-
-def create_arg_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--dataset_file", default='datasets/reviews.txt', type=str,
-                        help="Full dataset file to split (default datasets/reviews.txt)")
-    parser.add_argument("-s", "--sentiment", action="store_true",
-                        help="Do sentiment analysis (2-class problem)")
-    parser.add_argument("-tf", "--train_file", default='datasets/train.txt', type=str,
-                        help="Train file to learn from (default datasets/train.txt)")
-    parser.add_argument("-ttf", "--test_file", default='datasets/test.txt', type=str,
-                        help="Test file to evaluate on (default datasets/test.txt)")
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == "__main__":
-    args = create_arg_parser()
-
-    # TODO: comment
-    X, y = read_corpus(args.dataset_file, args.sentiment)
-
-    with open(args.dataset_file, encoding='utf-8') as f:
+@click.command()
+@click.argument('dataset_file', default=Path('datasets/reviews.txt'), type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, path_type=Path), help='Train file to learn from')
+@click.option('-tf', '--train_file', default=Path('datasets/train.txt'), type=click.Path(file_okay=True, dir_okay=False, writable=True, path_type=Path), help='Train data file')
+@click.option('-ttf', '--test_file', default=Path('datasets/test.txt'), type=click.Path(file_okay=True, dir_okay=False, writable=True, path_type=Path), help='Test data file')
+def main(
+    dataset_file: Path,
+    train_file: Path,
+    test_file: Path,
+) -> None:
+    with open(dataset_file, encoding='utf-8') as f:
         lines = [l for l in f]
 
     X_train, X_test = train_test_split(
         lines,
         train_size=0.9,
         random_state=42,
-        shuffle=True
+        shuffle=True,
     )
 
-    with open(args.train_file, 'w', encoding='utf-8') as f:
+    with open(train_file, 'w', encoding='utf-8') as f:
         f.write(''.join(X_train))
 
-    with open(args.test_file, 'w', encoding='utf-8') as f:
+    with open(test_file, 'w', encoding='utf-8') as f:
         f.write(''.join(X_test))
